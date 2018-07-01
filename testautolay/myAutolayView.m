9e9a46dadf9a2fc543078bc065609ad3066f9fb7
liangshun <linson@liangshundemac.local>
2018-07-02 06:08:49 +0800

    ok.

--- testautolay/myAutolayView.m
+++ testautolay/myAutolayView.m
@@ -10,14 +10,18 @@
 #import "tt.h"
 
 @implementation myAutolayView
+
 -(id)init
 {
     if(self=[super init])
     {
+        NSLog(@"count1:%@",[self.view_first valueForKey:@"retainCount"]);
         self.backgroundColor=[UIColor redColor];
         _view_first=[UIView new];
+        NSLog(@"count:%@",[self.view_first valueForKey:@"retainCount"]);
         _view_first.backgroundColor=[UIColor yellowColor];
         [self addSubview:_view_first];
+        NSLog(@"count2:%@",[self.view_first valueForKey:@"retainCount"]);
         
         _view_second=[UIView new];
         _view_second.backgroundColor=[UIColor greenColor];
@@ -27,19 +31,22 @@
         [self addSubview:_view_third];
         _view_third.backgroundColor=[UIColor blueColor];
     }
+    NSLog(@"count3:%@",[self.view_first valueForKey:@"retainCount"]);
     return self;
 }
 
 
-+ (BOOL)requiresConstraintBasedLayout {
++ (BOOL)requiresConstraintBasedLayout
+{
     return YES;
 }
 
 
 -(void)updateConstraints
 {
-    
+    NSLog(@"count4:%@",[self.view_first valueForKey:@"retainCount"]);
     [super updateConstraints];
+    NSLog(@"count5:%@",[self.view_first valueForKey:@"retainCount"]);
 }
 
 @end
