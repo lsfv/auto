68f004864ad68abc1ca31033586451505b743ef6
liangshun <linson@liangshundemac.local>
2018-07-01 10:11:52 +0800

    Initial Commit

---
+++ testautolay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testautolay
+//
+//  Created by liangshun on 18/7/1.
+//  Copyright © 2018年 liangshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testautolay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testautolay
+//
+//  Created by liangshun on 18/7/1.
+//  Copyright © 2018年 liangshun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
